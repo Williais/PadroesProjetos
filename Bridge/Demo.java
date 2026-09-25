00d69154eb6c333e33d04faab336ea2e588bc85f
williais <willian.padilha@academico.ifpb.edu.br>
2026-09-24 21:40:23 -0300

    Adicionando Mais 3 Padroes

---
+++ Bridge/Demo.java
@@ -0,0 +1,27 @@
+package Bridge;
+
+import Bridge.Device.Device;
+import Bridge.Device.Radio;
+import Bridge.Device.Tv;
+import Bridge.Remote.AdvancedRemote;
+import Bridge.Remote.BasicRemote;
+
+public class Demo {
+    public static void main(String[] args) {
+        testDevice(new Tv());
+        testDevice(new Radio());
+    }
+
+    public static void testDevice(Device device) {
+        System.out.println("Tests with basic remote.");
+        BasicRemote basicRemote = new BasicRemote(device);
+        basicRemote.power();
+        device.printStatus();
+
+        System.out.println("Tests with advanced remote.");
+        AdvancedRemote advancedRemote = new AdvancedRemote(device);
+        advancedRemote.power();
+        advancedRemote.mute();
+        device.printStatus();
+    }
+}
---
+++ Bridge/Remote/Remote.java
@@ -0,0 +1,13 @@
+package Bridge.Remote;
+
+public interface Remote {
+    void power();
+
+    void volumeDown();
+
+    void volumeUp();
+
+    void channelDown();
+
+    void channelUp();
+}
